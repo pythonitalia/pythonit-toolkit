ad1eff52d76ffff916ac4f919e5fe7687ca009f6
Marco Acierno <marcoaciernoemail@gmail.com>
2022-01-22 16:17:04 +0100

    Add new template for emails

--- pythonit_toolkit/emails/templates.py
+++ pythonit_toolkit/emails/templates.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class EmailTemplate(str, Enum):
     RESET_PASSWORD = "reset-password"
+    NEW_COMMENT_ON_SUBMISSION = "new-comment-on-submission"
 
     def __str__(self) -> str:
         return str.__str__(self)
