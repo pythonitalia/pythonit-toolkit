59297014bdf60ccbc4322085c5f869a060892feb
Marco Acierno <marcoaciernoemail@gmail.com>
2022-02-25 05:34:23 +0100

    New submission accepted email type

--- pythonit_toolkit/emails/templates.py
+++ pythonit_toolkit/emails/templates.py
@@ -4,6 +4,7 @@ from enum import Enum
 class EmailTemplate(str, Enum):
     RESET_PASSWORD = "reset-password"
     NEW_COMMENT_ON_SUBMISSION = "new-comment-on-submission"
+    SUBMISSION_ACCEPTED = "submission-accepted"
 
     def __str__(self) -> str:
         return str.__str__(self)
