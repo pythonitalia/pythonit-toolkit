cee5e4292a7d7cb294e4c9892b441bc38f12557f
Marco Acierno <marcoaciernoemail@gmail.com>
2022-02-26 00:29:23 +0100

    Add test for local support

--- tests/emails/backends/test_local.py
+++ tests/emails/backends/test_local.py
@@ -14,13 +14,15 @@ async def _():
             to="hello@world.it",
             subject="Hello World!",
             variables={"a": "b", "c": "d"},
+            reply_to=["test@placeholder.com"],
         )
 
-    assert mock_logger.call_count == 7
+    assert mock_logger.call_count == 8
     mock_logger.assert_any_call("=== Email sending ===")
     mock_logger.assert_any_call(f"Template: {str(EmailTemplate.RESET_PASSWORD)}")
     mock_logger.assert_any_call("From: from@from.it")
     mock_logger.assert_any_call("To: hello@world.it")
     mock_logger.assert_any_call("Subject: Hello World!")
     mock_logger.assert_any_call("Variables: {'a': 'b', 'c': 'd'}")
+    mock_logger.assert_any_call("Reply to: ['test@placeholder.com']")
     mock_logger.assert_any_call("=== End Email sending ===")
