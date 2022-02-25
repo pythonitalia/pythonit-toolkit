2234c3519938a06a471391004bdb13565b07a663
Marco Acierno <marcoaciernoemail@gmail.com>
2022-02-25 22:27:00 +0000

    Add new schedule invitation answer template (#5)

--- pythonit_toolkit/emails/templates.py
+++ pythonit_toolkit/emails/templates.py
@@ -5,6 +5,7 @@ class EmailTemplate(str, Enum):
     RESET_PASSWORD = "reset-password"
     NEW_COMMENT_ON_SUBMISSION = "new-comment-on-submission"
     SUBMISSION_ACCEPTED = "submission-accepted"
+    NEW_SCHEDULE_INVITATION_ANSWER = "new-schedule-invitation-answer"
 
     def __str__(self) -> str:
         return str.__str__(self)
