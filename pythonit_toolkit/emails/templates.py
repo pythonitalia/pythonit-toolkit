79fa6a170a53bf3d05292e0e1a2983eade1dadca
Marco Acierno <marcoaciernoemail@gmail.com>
2022-03-04 00:13:58 +0000

    New email template for schedule time change (#7)

--- pythonit_toolkit/emails/templates.py
+++ pythonit_toolkit/emails/templates.py
@@ -6,6 +6,7 @@ class EmailTemplate(str, Enum):
     NEW_COMMENT_ON_SUBMISSION = "new-comment-on-submission"
     SUBMISSION_ACCEPTED = "submission-accepted"
     NEW_SCHEDULE_INVITATION_ANSWER = "new-schedule-invitation-answer"
+    SUBMISSION_SCHEDULE_TIME_CHANGED = "submission-schedule-time-change"
 
     def __str__(self) -> str:
         return str.__str__(self)
